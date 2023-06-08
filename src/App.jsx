e29d07f76921f4c8be1d45b58ffa4550e99af9a1
R0su <2019-104574@rtu.edu.ph>
2023-06-08 22:37:52 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from './components/Hero'
 import AboutMe from './components/AboutMe'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
-import WhatIdo from './components/whatido'
+import WhatIdo from './components/WhatIDo'
 
 
 import './App.css'
