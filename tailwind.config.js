d09af5131f63528d517a860ccd7a03bfde4be038
R0su <2019-104574@rtu.edu.ph>
2023-06-11 19:58:14 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,15 @@ export default {
   ],
   theme: {
     screens: {
-      'sm': '720px',
-      // => @media (min-width: 640px) { ... } 
-  
-      'md': '1024px',
-      // => @media (min-width: 768px) { ... }
+      'sm': {'min' : '720px','max' : '1023px'},
+      
+      'sl': {'min' : '1024px','max' : '1439px'},
+      
+      'md': {'min' : '1440px','max' : '2560px'},
+
+      'lg': {'min' : '2560px'},
+
+     
     },
     extend: {
       fontFamily: {
