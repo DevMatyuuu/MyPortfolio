a5eeaf65b953983c30d16b9da3ae69c48228a883
R0su <2019-104574@rtu.edu.ph>
2023-06-11 20:25:57 +0800

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -20,19 +20,8 @@ function Projects() {
   return (
     <section id='Projects'>
         <div className='h-screen w-auto py-20'>
-          <Typography className="font-poppins text-4xl font-bold text-center uppercase text-slate-600 md:py-10 md:mb-10">my projects</Typography>
-        <div className='grid md:text-sm grid-cols-1 gap-2 md:gap-7 md:ml-2 text-center font-semibold uppercase'>
-               {proj.map((item) => ( 
-                <Card key={item.id} className='mt-6 md:w-100 md:h-80 md:max-w-[700px]  justify-center transition-transform duration-300 transform shadow-md' >
-                  <CardBody>
-                    <Typography className='mt-2'>{item.title}</Typography>
-                    <Typography className='mt-2'></Typography>
-                  </CardBody>
-                </Card>
-                ))}
-              </div>
-              </div>
-
+          <Typography className="font-poppins text-4xl font-bold text-center uppercase text-slate-600 md:py-10 md:mb-10">my projects (Currently Building)</Typography>
+       </div>
 
     </section>
   )
